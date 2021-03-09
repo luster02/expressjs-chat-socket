8be7c4ebe1c85b252feef00ea3ad35b15a0d97a5
luster 02 <bombolti_02@hotmail>
2021-03-08 22:35:22 -0600

    [ADD]: sockets functions

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,15 +1,23 @@
 import { Application } from 'express'
+import { Server } from 'socket.io'
 
 import main from './main.routes'
 import { AuthRoutes } from './auth/auth.routes'
 import { UserRoutes } from './user/user.routes'
+import { MessageRoutes } from './messages/message.routes'
 
-export function modules(app: Application) {
+function sockets(server?: Server, functions?: any) {
+    server?.on('connection', client => {
+        functions(client)
+    })
+}
 
-    app.use(main())
+export function modules(app: Application, server?: Server) {
+
+    app.use('/', main())
     app.use('/auth', AuthRoutes())
-    app.use('/user', UserRoutes())
-
+    app.use('/user', UserRoutes(sockets, server))
+    app.use('/messages', MessageRoutes(sockets, server))
 
     return app
 }
