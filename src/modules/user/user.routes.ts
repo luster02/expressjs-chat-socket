98fa1310daf43df51ccb564a9dd253c82699cfb4
luster 02 <bombolti_02@hotmail>
2021-03-08 22:34:44 -0600

    [ADD]: user sockets

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,14 +1,31 @@
 import { Router } from 'express'
-import { editUser, getUser, getUserByEmail, getUsers } from './user.controller'
+import { Server } from 'socket.io'
+import { editUser, getUser, getUserByEmail, getUsers, updateUserStatus } from './user.controller'
 import { ValidateJsonWenToken } from '../../middlewares/jwt-validator'
+import { verifyJwt } from '../../utils/jwt'
 
-export function UserRoutes() {
+export function UserRoutes(sockets?: any, server?: Server) {
     const router = Router()
 
     router.get('/', ValidateJsonWenToken, getUsers)
     router.get('/:id', ValidateJsonWenToken, getUser)
     router.get('/:email', ValidateJsonWenToken, getUserByEmail)
     router.post('/edit', ValidateJsonWenToken, editUser)
+
+    sockets(server, (client: any) => {
+        const [valid, uid] = verifyJwt(client.handshake.headers['authorization'])
+
+        if (!valid) { return client.disconnect(); }
+
+        updateUserStatus(uid, true)
+
+        client.join(uid)
+
+
+        client.on('disconnect', () => {
+            updateUserStatus(uid, false)
+        })
+    })
 
     return router
 }
